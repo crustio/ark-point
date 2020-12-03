af4e83dc45ad9277ab9453b4c08ff16f44bc02a3
yuhui <1161569428@qq.com>
2020-12-03 21:54:49 +0800

    add log config

---
+++ src/log/index.ts
@@ -0,0 +1,24 @@
+import {createLogger, format, transports} from 'winston';
+
+export const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        format.colorize(),
+        format.errors({stack: true}),
+        format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)
+    ),
+    transports: [
+        //
+        // - Write to all logs with level `info` and below to `crust-api-combined.log`.
+        // - Write all logs error (and below) to `crust-api-error.log`.
+        //
+        new transports.Console(),
+        new transports.File({filename: 'logs/ark-point-error.log', level: 'error'}),
+        new transports.File({filename: 'logs/ark-point-info.log', level: 'info'}),
+        new transports.File({filename: 'logs/ark-point-debug.log', level: 'debug'}),
+        new transports.File({filename: 'logs/ark-backend-combined.log'}),
+    ],
+});
