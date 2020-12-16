565bba74c23d3f8f1dd1b22a0646fb621a01c36d
Zikun Fan <zikunvan@gmail.com>
2020-12-16 19:15:19 +0800

    Fix yarn.lock and lint

--- src/db/index.ts
+++ src/db/index.ts
@@ -208,7 +208,8 @@ export const historyPubKeyReports = async (currentReportSlot: number) => {
       },
     },
   ])
-      .option({ allowDiskUse: true }).exec();
+    .option({allowDiskUse: true})
+    .exec();
   return results;
 };
 
